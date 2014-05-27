1a74e6d32c624f9f64ac1cffbf0cf2b060abc87f
MARCO <lefollmarc@hotmail.fr>
2014-05-27 14:40:12 +0200

    restart done

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,5 @@
 class Company < ActiveRecord::Base
+  belongs_to :hq_address, class: 'Address'
+  belongs_to :billing_address, class: 'Address'
+  has_many :pictures
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,8 @@
 class Picture < ActiveRecord::Base
   belongs_to :company
+
+  has_attached_file :file,
+
+  styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :file, :content_type => /\Aimage\/.*\Z/
 end
