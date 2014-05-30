36cdaedb567ccc5f82054aab1c3615a6b4d37cb5
MARCO <lefollmarc@hotmail.fr>
2014-05-30 09:27:16 +0200

    fix logo picture in company adminuser

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,4 +6,9 @@ class Company < ActiveRecord::Base
     styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :banner,
     :content_type => /\Aimage\/.*\Z/
+
+    has_attached_file :logo,
+      styles: { medium: "300x300>", thumb: "100x100>" }
+    validates_attachment_content_type :logo,
+      :content_type => /\Aimage\/.*\Z/
 end
