0fbf2dd5cb9b6c529b457cc9a541518f2b738251
MARCO <lefollmarc@hotmail.fr>
2014-05-30 12:54:26 +0200

    create table founders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530072106) do
+ActiveRecord::Schema.define(version: 20140530105131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,5 +80,17 @@ ActiveRecord::Schema.define(version: 20140530072106) do
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
   end
+
+  create_table "founders", force: true do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "email"
+    t.string   "picture"
+    t.integer  "company_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "founders", ["company_id"], name: "index_founders_on_company_id", using: :btree
 
 end
