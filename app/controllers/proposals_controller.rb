6a64b287682b1a9f9ed43c78aa495e36cf3afde7
MARCO <lefollmarc@hotmail.fr>
2014-06-05 13:23:07 +0200

    add proposal to user no proposal to campaign

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -3,10 +3,13 @@ class ProposalsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    current_user.proposals.create
+    current_user.proposals.create!(proposal_params)
+    redirect_to params[:proposal][:return_to]
   end
 
   private
-
+  def proposal_params
+    params.require(:proposal).permit(:amount, :rate, :proposal)
+  end
 
 end
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,5 @@
 class Campaign < ActiveRecord::Base
   belongs_to :company
+  has_many :proposals
   acts_as_commentable
 end
