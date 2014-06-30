40811744c61385c5b6e1553fd7244086d0eaa930
Marco <lefollmarc@hotmail.fr>
2014-06-30 15:04:19 +0200

    configuration of gem devise like omr

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,10 @@ module Boostech
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+
+    # Require for Heroku
+    config.assets.initialize_on_precompile = false
+
     config.i18n.default_locale = :fr
   end
 end
